00bb0ffc99e96b7f203242e090aed5da1a62c25f
ugurtansal <ugur.tansal@gmail.com>
2025-03-08 13:02:54 +0300

    Second principle O (Open/Closed Principle) applied

---
+++ src/CreditCardPayment.java
@@ -0,0 +1,6 @@
+public class CreditCardPayment implements  PaymentMethod{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Payment made by credit card: " + amount);
+    }
+}
---
+++ src/PayPalPayment.java
@@ -0,0 +1,6 @@
+public class PayPalPayment implements PaymentMethod{
+    @Override
+    public void pay(double amount) {
+        System.out.println("Payment made by PayPal: " + amount);
+    }
+}
